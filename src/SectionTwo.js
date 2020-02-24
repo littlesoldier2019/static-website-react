3185b844220445e570744d8dad8f8d2ae333409b
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-24 12:47:16 +0200

    Created Section Two components

---
+++ src/SectionTwo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SectionTwoList from "./SectionTwoList";
+
+const SectionTwo = () => {
+    return (
+        <SectionTwoList/>
+    )
+}
+
+export default SectionTwo
---
+++ src/SectionTwoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SectionTwoItem from "./SectionTwoItem";
+
+const SectionTwoList = () => {
+    return (
+        <div className="container_first">
+            <SectionTwoItem titleOne='SUPERBLY' titleTwo='RESPONSIVE' src='./image/smartphone.png'/>
+            <SectionTwoItem titleOne='QUEEKY' titleTwo='CLEAN' src='./image/smartphone.png'/>
+            <SectionTwoItem titleOne='MULTI' titleTwo='PURPOSE' src='./image/smartphone.png'/>
+            <SectionTwoItem titleOne='HIGHLY' titleTwo='FLEXIBLE' src='./image/smartphone.png'/>
+        </div>
+    )
+};
+
+export default SectionTwoList
