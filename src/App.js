631c3be62c68d5e331f9fad95d24b41886de6f00
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-24 19:43:50 +0200

    Added images, finished my part

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import SectionTwo from "./SectionTwo";
 
 function App()
 {
@@ -9,7 +7,7 @@ function App()
     <div className='wrapper'>
       <div className="head">
         <div className="container">
-            <SectionTwo/>
+
         </div>
       </div>
     </div>
--- src/SectionTwoList.js
+++ src/SectionTwoList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import SectionTwoItem from "./SectionTwoItem";
 import './SectionTwoList.css';
+import smartphone from '../src/assets/smartphone.png';
+import monitor from '../src/assets/monitor.jpg';
+import laptop from '../src/assets/laptop.jpg';
+import recycle from '../src/assets/recycle.png';
+
 const SectionTwoList = () => {
     return (
         <div className="container_first">
-            <SectionTwoItem titleOne='SUPERBLY' titleTwo='RESPONSIVE' src='./image/smartphone.png'/>
-            <SectionTwoItem titleOne='QUEEKY' titleTwo='CLEAN' src='./image/smartphone.png'/>
-            <SectionTwoItem titleOne='MULTI' titleTwo='PURPOSE' src='./image/smartphone.png'/>
-            <SectionTwoItem titleOne='HIGHLY' titleTwo='FLEXIBLE' src='./image/smartphone.png'/>
+            <SectionTwoItem titleOne='SUPERBLY' titleTwo='RESPONSIVE' src={smartphone}/>
+            <SectionTwoItem titleOne='QUEEKY' titleTwo='CLEAN' src={monitor}/>
+            <SectionTwoItem titleOne='MULTI' titleTwo='PURPOSE' src={laptop}/>
+            <SectionTwoItem titleOne='HIGHLY' titleTwo='FLEXIBLE' src={recycle}/>
         </div>
     )
 };
