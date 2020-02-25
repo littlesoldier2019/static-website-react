dc5c3db6e5fbc3bbd89e242e0edd3f1e566ab16c
stbugyei <stbugyei@yahoo.co.uk>
2020-02-25 15:20:08 +0200

    first commit

---
+++ Source/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./components/layout/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
